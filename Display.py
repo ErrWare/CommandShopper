d339c11281efa6c88768c5ae7a545c6c1bc7c7d0
errware <ilanbuzzetti@gmail.com>
2018-10-08 08:18:42 -0500

    changing display_items fields param works, TODO: decide how to store and display floats/dates

--- Display.py
+++ Display.py
@@ -1,63 +1,74 @@
 from assets.DictClusterer import DictClusterer
+from assets.shoplib import Item, Category
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
+
 class Display:
+    i_columns = Item('item',
+                      'ID',
+                      'base$',
+                      'base amt',
+                      'unit',
+                      'sale$',
+                      'sale text',
+                      'sale ends',
+                      'categories')
+
     def setShop(self, shop):
         self.shop = shop
-    
+
     # sort_on id, name, namelength
     def displayCategories(self, sort_on='id'):
         if sort_on == 'id':
-            keyfunc = lambda c: int(c.ID)
+            def keyfunc(c): return int(c.ID)
         elif sort_on == 'name':
-            keyfunc = lambda c: c.name
+            def keyfunc(c): return c.name
         elif sort_on == 'len':
-            keyfunc = lambda c: len(c.name)
+            def keyfunc(c): return len(c.name)
         else:
             keyfunc = None
         cats = self.shop.categories
         cats.sort(key=keyfunc)
         max_width = max(len(cat.name) for cat in cats)
-        
+
         name_header = ' Category Name '
         id_header = ' id '
         max_width = max(max_width, len(name_header))
         unf_header = '{:<'+str(max_width)+'}'+' '*5+'{:>4}'
-        print(unf_header.format(name_header,id_header))
-        print(unf_header.format('-'*len(name_header),'-'*len(id_header)))
+        print(unf_header.format(name_header, id_header))
+        print(unf_header.format('-'*len(name_header), '-'*len(id_header)))
         for cat in cats:
             width = len(cat.name)
-            print(cat.name + ' ' + '.' * (5+max_width-width) + '{:>3}'.format(cat.ID))
-            
-    def displayItems(self, fields=['name','ID']):
-        col_widths = [len(field) for field in fields]
+            print(cat.name + ' ' + '.' *
+                  (5+max_width-width) + '{:>3}'.format(cat.ID))
+
+    def displayItems(self, fields=['name', 'ID']):
+        col_widths = [len(str(field)) for field in fields]
         items = self.shop.items
         items = items[:20]
-        logging.info('tup type: {}'.format(type(items[0])))
-        logging.info('tup dict: {}'.format(items[0].name))
         for item in items:
             for index, field in enumerate(fields):
-                col_widths[index] = max(col_widths[index], len(getattr(item,field)))
+                col_widths[index] = max(
+                    col_widths[index], len(str(getattr(item, field))))
         # Print Header
-        formatted_col_names = [('{:^'+str(width)+'}').format(field)        \
+        formatted_col_names = [('{:^'+str(width)+'}').format(getattr(Display.i_columns,field))
                                for field, width in zip(fields, col_widths)]
         header = (4*' ').join(formatted_col_names)
         print(header)
 
         # Print Items
         for item in items:
-            for field, width in zip(fields,col_widths):
-                field_val = str(getattr(item,field))
+            for field, width in zip(fields, col_widths):
+                field_val = str(getattr(item, field))
                 print(field_val, (width-len(field_val))*'.', end=4*'.')
             print()
-        
 
     '''
 
     '''
     # DATA EXPLORATION
-    
+
     '''
     def display_shopping_info(self):
         logging.debug('displaying shopping info')
--- Shop.py
+++ Shop.py
@@ -8,38 +8,36 @@ from pprint import pprint
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
-#logging.disable(level=logging.CRITICAL)
+# logging.disable(level=logging.CRITICAL)
 
-# DID:
-# replace self.items with self._items
-# make @property items to return self._items or filtered_items
-# make inputs dic input as the namedtuples
 
 class Shop(object):
-    ASSETS_DIR  = 'assets'
-    CATS_FILE   = '_cats.json'
-    ITEM_FILE   = '_items.json'
-    SHOP_FILE   = '_shopping_list.json'
+    ASSETS_DIR = 'assets'
+    CATS_FILE = '_cats.json'
+    ITEM_FILE = '_items.json'
+    SHOP_FILE = '_shopping_list.json'
     HEADER_FILE = '_headers.json'
-    URL_FILE    = '_url.json'    #unformatted category url
-    Item = namedtuple('Item_Fields',['name','id',
-                               'base_price','base_quantity',
-                               'base_unit',
-                               'sale_price','promo_tag',
-                               'sale_end_date'])
+    URL_FILE = '_url.json'  # unformatted category url
+    Item = namedtuple('Item_Fields', ['name', 'id',
+                                      'base_price', 'base_quantity',
+                                      'base_unit',
+                                      'sale_price', 'promo_tag',
+                                      'sale_end_date'])
 
-    #item_fields as tuple of (name, base price, base unit, display uom, ..?)
+    # item_fields as tuple of (name, base price, base unit, display uom, ..?)
     def __init__(self, name):
-        
+
         self.name = name.lower()
         try:
-            with open(os.path.join(Shop.ASSETS_DIR, self.name + Shop.CATS_FILE),'r') as cat_file:
+            with open(os.path.join(Shop.ASSETS_DIR, self.name + Shop.CATS_FILE), 'r') as cat_file:
                 self._categories = json.load(cat_file)
-                self._categories = {ID: Category(*cat) for ID, cat in self._categories.items()}
-            with open(os.path.join(Shop.ASSETS_DIR, self.name + Shop.ITEM_FILE),'r') as item_file:
+                self._categories = {ID: Category(
+                    *cat) for ID, cat in self._categories.items()}
+            with open(os.path.join(Shop.ASSETS_DIR, self.name + Shop.ITEM_FILE), 'r') as item_file:
                 self._items = json.load(item_file)
-                self._items = {ID: Item(*val) for ID, val in self._items.items()}
-            with open(os.path.join(Shop.ASSETS_DIR, self.name + Shop.HEADER_FILE),'r') as header_file:
+                self._items = {ID: Item(*val)
+                               for ID, val in self._items.items()}
+            with open(os.path.join(Shop.ASSETS_DIR, self.name + Shop.HEADER_FILE), 'r') as header_file:
                 self.header = json.load(header_file)
             with open(os.path.join(Shop.ASSETS_DIR, self.name + Shop.URL_FILE), 'r') as url_file:
                 self.unf_url = json.load(url_file)
@@ -47,15 +45,13 @@ class Shop(object):
             print('Error opening sprouts inventory\nexception: {}'.format(e))
             raise e
         try:
-            with open(os.path.join(Shop.ASSETS_DIR, self.name + Shop.SHOP_FILE),'r') as shopping_list_file:
+            with open(os.path.join(Shop.ASSETS_DIR, self.name + Shop.SHOP_FILE), 'r') as shopping_list_file:
                 self.shopping_list = json.load(shopping_list_file)
         except:
             logging.info('No shopping list detected, creating empty list')
             self.shopping_list = []
         logging.debug('completed {} __init__'.format(self.name))
-    
 
-        
     '''
     ACCESSING
 
@@ -69,12 +65,11 @@ class Shop(object):
     def categories(self):
         return list(self._categories.values())
 
-    def filter_items(self,filters=None, _cache={'filters':None}):
+    def filter_items(self, filters=None, _cache={'filters': None}):
         if filters != _cache['filters']:
-            self._filtered_items = [item for item in self._items.values() if 
-                                  any(Shop.item_matches(item,f) for f in filters)]
+            self._filtered_items = [item for item in self._items.values() if
+                                    any(Shop.item_matches(item, f) for f in filters)]
             _cache['filters'] = filters
-
 
     '''
     PROCESSING
@@ -82,15 +77,15 @@ class Shop(object):
     '''
     @staticmethod
     def filter_dic(dic, filters):
-        return [(key,value) for key, value in dic.items()
-                            for f in filters
-                            if Shop.item_matches(value, f)]
+        return [(key, value) for key, value in dic.items()
+                for f in filters
+                if Shop.item_matches(value, f)]
 
-    @staticmethod                
+    @staticmethod
     def item_matches(item, filter_):
         if filter_ is None:
             return True
         elif filter_.isdigit():
             return filter_ in item.categories or filter_ == item.ID
         else:
-           return re.search(filter_, item.name) is not None
+            return re.search(filter_, item.name) is not None
